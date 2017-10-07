da2f4c460fb2c2163d5b74a28d9598f95e35ab49
xinpengHuang <32598435+xinpengHuang@users.noreply.github.com>
2017-10-07 13:06:24 -0400

    Update Longest Substring with At Most K Distinct Characters.java

--- Longest Substring with At Most K Distinct Characters.java
+++ Longest Substring with At Most K Distinct Characters.java
@@ -1,15 +1,15 @@
-
-
 ```
-
 /*
+Description
 Given a string, find the length of the longest substring T that contains at most k distinct characters.
+For example, Given s = “eceba” and k = 2, T is "ece" which its length is 3.
 
-For example, Given s = “eceba” and k = 2,
-
-T is "ece" which its length is 3.
+Thoughts
+Solution 1: O(N) time and O(1) space, using sliding window with two pointers 
+Solution 2: O(N) time and O(k) space, using sliding window with two pointers and LinkedHashMap
 */
 
+// Solution 1
 class Solution {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
         int start = 0;
@@ -34,4 +34,29 @@ class Solution {
     }
 }
 
+// Solution 2
+class Solution {
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        if (k == 0) {
+            return 0;
+        }
+        // record the last location for each character in the sliding window
+        // arrange them in an access-ordered LinkedHashMap
+        Map<Character, Integer> map = new LinkedHashMap<Character, Integer>(k + 1, (float) 0.75, true);
+        int start = 0;
+        int length = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (!map.containsKey(s.charAt(i)) && map.size() == k) {
+                // the first entry given from the iterator is the eldest entry
+                Iterator<Map.Entry<Character, Integer>> iterator = map.entrySet().iterator();
+                int value = iterator.next().getValue();
+                start = value + 1;
+                iterator.remove();
+            }
+            map.put(s.charAt(i), i);
+            length = Math.max(length, i - start + 1);
+        }
+        return length;
+    }
+}
 ```
