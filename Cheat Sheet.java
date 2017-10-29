3fd6cfa7f39f73f3b26e40d6f89cf40416e610d4
xinpengHuang <32598435+xinpengHuang@users.noreply.github.com>
2017-10-28 21:27:06 -0400

    Update Cheat Sheet.java

--- Cheat Sheet.java
+++ Cheat Sheet.java
@@ -1,31 +1,31 @@
 ```
-  Integer
-  Character
-  StringBuilder
-  
-  Set<> set = new HashSet<>();
-  TreeSet<> set = new TreeSet<>();  
-  Map<,> map = new HashMap<,>();
-  Map<,> map = new LinkedHashMap<,>();
-  Map<,> map = new TreeMap<,>();  
-  Deque<> deque = new LinkedList<>();
-  Queue<> queue = new LinkedList<>();
-  Queue<> queue = new PriorityQueue<>();
-  Queue<> queue = new PriorityQueue<>(new Comparator<>() {
-    public int compare(a, b) {
-      
-      return;
-    }
-  });
-  
-  for (int i = 0; i < n; i++) {
-    
+Integer
+Character
+StringBuilder
+
+Set<> set = new HashSet<>();
+TreeSet<> set = new TreeSet<>();  
+Map<,> map = new HashMap<,>();
+Map<,> map = new LinkedHashMap<,>();
+Map<,> map = new TreeMap<,>();  
+Deque<> deque = new LinkedList<>();
+Queue<> queue = new LinkedList<>();
+Queue<> queue = new PriorityQueue<>();
+Queue<> queue = new PriorityQueue<>(new Comparator<>() {
+  public int compare(a, b) {
+
+    return;
   }
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; i++) {
-    
-    }
+});
+
+for (int i = 0; i < n; i++) {
+
+}
+for (int i = 0; i < n; i++) {
+  for (int j = 0; j < m; i++) {
+
   }
-  
-  
+}
+
+
 ```
