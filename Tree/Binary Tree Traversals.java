7c3164f053f072a5756c609be61a1664cd37f306
xinpengHuang <32598435+xinpengHuang@users.noreply.github.com>
2017-10-29 22:21:35 -0400

    Update Binary Tree Traversals.java

--- Tree/Binary Tree Traversals.java
+++ Tree/Binary Tree Traversals.java
@@ -1,10 +1,36 @@
 ```
+
+/*
+Description
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+Thoughts
+Solution 1: O(n) time and O(n) space, using queue.
+*/
+
+// Solution 1
+
+
 /*
 Description
 Given a binary tree, return the preorder traversal of its nodes' values.
 
 Thoughts
-Solution 1: O(n) time and O(n) space, using recursion.
+Solution 1: O(n) time and O(n) space, using stack.
 */
 
 // Solution 1
@@ -32,7 +58,7 @@ Description
 Given a binary tree, return the inorder traversal of its nodes' values.
 
 Thoughts
-Solution 1: O(n) time and O(n) space, using recursion.
+Solution 1: O(n) time and O(n) space, using stack.
 */
 
 // Solution 1
